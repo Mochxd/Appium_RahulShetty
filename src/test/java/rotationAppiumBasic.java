e654bf81c6021aec18da184c0703455739763bbb
Mohamed Mostafa <122634626+Mochxd@users.noreply.github.com>
2024-08-11 21:16:05 +0300

    Using Activity class to direct to a specific page

--- src/test/java/rotationAppiumBasic.java
+++ src/test/java/rotationAppiumBasic.java
@@ -1,7 +1,11 @@
+import com.google.common.collect.ImmutableMap;
 import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.Activity;
 import io.appium.java_client.android.nativekey.AndroidKey;
 import io.appium.java_client.android.nativekey.KeyEvent;
 import org.openqa.selenium.DeviceRotation;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.remote.RemoteWebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -11,11 +15,15 @@ import java.net.MalformedURLException;
 public class rotationAppiumBasic extends BaseTest{
     @Test
      public void AppiumTest() throws MalformedURLException {
-        androidDriver.findElement(AppiumBy.accessibilityId("Preference")).click();
-        androidDriver.findElement(AppiumBy.xpath("//android.widget.TextView[@content-desc=\"3. Preference dependencies\"]")).click();
-        androidDriver.findElement(AppiumBy.id("android:id/widget_frame")).click();
+//        androidDriver.findElement(AppiumBy.accessibilityId("Preference")).click();
+//        androidDriver.findElement(AppiumBy.xpath("//android.widget.TextView[@content-desc=\"3. Preference dependencies\"]")).click();
+
+       Activity activity = new Activity("io.appium.android.apis","io.appium.android.apis.preference.PreferenceDependencies");
+        ((JavascriptExecutor) androidDriver).executeScript("mobile: startActivity",
+                ImmutableMap.of("intent","io.appium.android.apis/io.appium.android.apis.preference.PreferenceDependencies"));
         DeviceRotation rotation = new DeviceRotation(0,0,90);
         androidDriver.rotate(rotation);
+        androidDriver.findElement(AppiumBy.id("android:id/widget_frame")).click();
         androidDriver.findElement(AppiumBy.xpath("(//android.widget.RelativeLayout)[2]")).click();
         String title = androidDriver.findElement(AppiumBy.id("android:id/alertTitle")).getText();
         Assert.assertEquals(title,"WiFi settings");
