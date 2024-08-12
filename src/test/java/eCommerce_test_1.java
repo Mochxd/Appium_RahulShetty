08e7d8804f24baa604fd7bd45a9fe470334fd7ec
Mohamed Mostafa <122634626+Mochxd@users.noreply.github.com>
2024-08-12 22:34:15 +0300

    Check the negative scenario when the user doesn't enter a name

--- src/test/java/eCommerce_test_1.java
+++ src/test/java/eCommerce_test_1.java
@@ -1,4 +1,5 @@
 import io.appium.java_client.AppiumBy;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class eCommerce_test_1 extends BaseTest{
@@ -12,5 +13,17 @@ public class eCommerce_test_1 extends BaseTest{
                         + "new UiSelector().text(\"Egypt\"));")).click();
         androidDriver.findElement((AppiumBy.id("com.androidsample.generalstore:id/radioFemale"))).click();
         androidDriver.findElement(AppiumBy.id("com.androidsample.generalstore:id/btnLetsShop")).click();
+    }
+    @Test
+    public void fillFormWithoutEnteringName() throws InterruptedException {
+        androidDriver.findElement(AppiumBy.id("android:id/text1")).click();
+        androidDriver.findElement(AppiumBy.androidUIAutomator(
+                "new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView("
+                        + "new UiSelector().text(\"Egypt\"));")).click();
+        androidDriver.findElement((AppiumBy.id("com.androidsample.generalstore:id/radioFemale"))).click();
+        androidDriver.findElement(AppiumBy.id("com.androidsample.generalstore:id/btnLetsShop")).click();
+        String toastMessage = androidDriver.findElement(AppiumBy.xpath("//android.widget.Toast")).getAttribute("name");
+        System.out.printf(toastMessage);
+        Assert.assertEquals(toastMessage,"Please enter your name");
     }
 }
