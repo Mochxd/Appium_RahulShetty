5d5cba0216d85909b24293a98abacb54fc6c78e8
Mohamed Mostafa <122634626+Mochxd@users.noreply.github.com>
2024-08-12 22:05:43 +0300

    add the new test for new e-commerce app

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -28,9 +28,10 @@ public class BaseTest {
 
         UiAutomator2Options options = new UiAutomator2Options();
         options.setDeviceName("Mostafa");
-        options.setApp("D:\\Appium\\Appium_RahulShetty\\src\\main\\resources\\ApiDemos-debug.apk");
-         androidDriver = new AndroidDriver(new URL("http://127.0.0.1:4723/"),options);
-         androidDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        //options.setApp("D:\\Appium\\Raul\\src\\main\\resources\\ApiDemos-debug.apk");
+        options.setApp("D:\\Appium\\Raul\\src\\main\\resources\\General-Store.apk");
+        androidDriver = new AndroidDriver(new URL("http://127.0.0.1:4723/"),options);
+        androidDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
     public void longPress(WebElement ele){
         ((JavascriptExecutor) androidDriver).executeScript("mobile: longClickGesture",
